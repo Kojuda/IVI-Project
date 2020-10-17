7678f60613b508dad52129204f3f7ff1a5e1d97f
Kojuda <d4nnykohler@gmail.com>
2020-10-17 19:32:36 +0200

    Work on SQLite Database

--- ressources/db.py
+++ ressources/db.py
@@ -9,15 +9,44 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
+
+# class Url(Base):
+#     __tablename__ = 'urls'
+#     id = Column(Integer, primary_key=True)
+#     url = Column(String, nullable=False)
+#     status = Column(Integer, default=0)
+#     date_created = Column(DateTime, default=datetime.datetime.now(), nullable=False)
+#     date_updated = Column(DateTime, onupdate=datetime.datetime.now())
+
+# def insertURL(session, url):
+#     url = Url(url=url)
+#     session.add(url)
+#     session.commit()
+
+# def updateURL(session, url_object, newStatus=1):
+#     url_object.status = newStatus
+#     session.commit()
+
+#~~~~~~~~~~~~~~~~~~~~~Connect the database~~~~~~~~~~~~~~~~~~~~~
 Base = declarative_base()
 
-class Url(Base):
-    __tablename__ = 'urls'
+engine = create_engine('sqlite:///DATABASES/project.db') #, echo=True pour les log
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+#~~~~~~~~~~~~~~~~~~~~~PROJET~~~~~~~~~~~~~~~~~~~~~
+
+class Urls_ads(Base):
+    __tablename__ = 'urls_ads'
     id = Column(Integer, primary_key=True)
     url = Column(String, nullable=False)
     status = Column(Integer, default=0)
     date_created = Column(DateTime, default=datetime.datetime.now(), nullable=False)
     date_updated = Column(DateTime, onupdate=datetime.datetime.now())
+    country= Column(String, nullable=False)
+    ad_number=Column(String, nullable=False)
 
 def insertURL(session, url):
     url = Url(url=url)
@@ -27,8 +56,3 @@ def insertURL(session, url):
 def updateURL(session, url_object, newStatus=1):
     url_object.status = newStatus
     session.commit()
-
-engine = create_engine('sqlite:///urlToCrawl.db') #, echo=True pour les log
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
