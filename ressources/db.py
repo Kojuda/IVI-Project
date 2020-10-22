55b5bbb60899fafa84bf8763ff6ec49000047f16
Jasmin Wyss <jasmin.wyss@unil.ch>
2020-10-22 23:57:01 +0200

    Modified database after running the script again

--- ressources/db.py
+++ ressources/db.py
@@ -11,22 +11,22 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 
-# class Url(Base):
-#     __tablename__ = 'urls'
-#     id = Column(Integer, primary_key=True)
-#     url = Column(String, nullable=False)
-#     status = Column(Integer, default=0)
-#     date_created = Column(DateTime, default=datetime.datetime.now(), nullable=False)
-#     date_updated = Column(DateTime, onupdate=datetime.datetime.now())
+class Url():
+     __tablename__ = 'urls'
+     id = Column(Integer, primary_key=True)
+     url = Column(String, nullable=False)
+     status = Column(Integer, default=0)
+     date_created = Column(DateTime, default=datetime.datetime.now(), nullable=False)
+     date_updated = Column(DateTime, onupdate=datetime.datetime.now())
 
-# def insertURL(session, url):
-#     url = Url(url=url)
-#     session.add(url)
-#     session.commit()
+def insertURL(session, url):
+     url = Url(url=url)
+     session.add(url)
+     session.commit()
 
-# def updateURL(session, url_object, newStatus=1):
-#     url_object.status = newStatus
-#     session.commit()
+def updateURL(session, url_object, newStatus=1):
+     url_object.status = newStatus
+     session.commit()
 
 #~~~~~~~~~~~~~~~~~~~~~Create de base~~~~~~~~~~~~~~~~~~~~~
 Base = declarative_base()
