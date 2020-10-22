65eef677cdfa011511f0a057fe4a40c5c90a9e2c
Jasmin Wyss <jasmin.wyss@unil.ch>
2020-10-22 23:54:13 +0200

    Trying out the getArticles.py function: new pages added to the URL database, Started fiddeling with second etape [is nowhere near ready to be deployed]

---
+++ Etape2getURL.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: T. Pineau
+# modified & adapted: Luisa Rogrigues & Jasmin Wyss
+# creation: 06.10.2020
+# But extraire tous les urls
+import time, json, random
+from ressources.webdriver import Chrome, Firefox #fichier selenium_driver.py à placer dans le même dossier que votre script
+from ressources.documentation import Documentation # fichier documentation.py qui se trouve dans le dossier ressources
+from ressources.db import session, Url #fichier db.py  qui se trouve dans le dossier ressources
+
+def saveData(browser, filename_prefix='selenium'):
+    '''Fonction pour l'exemple qui enregistre le code client, la capture d'écran et code serveur'''
+    browser.clientCode('./results/html/'+filename_prefix+'_clientCode.html')
+    browser.serverCode('./results/html/' + filename_prefix + '_serverCode.html')
+    browser.screenshot('./results/screenshots/'+filename_prefix+'_screenshot.png', width=1080) #on fixe la largeur de la fenêtre avec width
+
+if __name__ == '__main__':
+    doc = Documentation()#Crée la documentation
+    # ~~~~~~~~~~~~~~~ Début Selenium ~~~~~~~~~~~~~~~ #
+    browser = Firefox(tor=False, headless=False)
+
+    # ~~~~~~~~~~~~~~~ Récupération des URL à parcourir ~~~~~~~~~~~~~ #
+    #ToDo:  Solve for this
+    for i in session.query(Url).filter_by(status=0):
+        #Pour tous les urls dans la database qui ne sont pas encore extrait
+        doc.info['selenium'] = []
+        info = browser.get(i.url)
+        doc.info['selenium'].append(info)
+        saveData(browser, filename_prefix=str(i.id))
+        with open('./results/documentation/'+str(i.id)+'_documentation.json', 'wb') as f:
+            f.write(str(doc).encode('utf-8'))
+        updateURL(session, i) #met à jour le status de l'URL
+        time.sleep(random.uniform(1.5, 2.5)) #attente entre 1.5 et 2.5 sec
