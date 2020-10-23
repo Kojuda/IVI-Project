7f7b0a060ff8521f76b805f61f68599fc522198d
Jasmin Wyss <jasmin.wyss@unil.ch>
2020-10-23 14:23:02 +0200

    Version 1 qui marche et les premiers codes/screenshots fait =)<3 Continuation sur l'ordi a luisa

--- getCodes.py
+++ getCodes.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python
 # coding=utf-8
 # author: T. Pineau
-# modified & adapted: Luisa Rogrigues & Jasmin Wyss
+# modified & adapted: Luisa Rodrigues & Jasmin Wyss
 # creation: 06.10.2020
 # But extraire tous les urls
 import time, json, random
+from sqlalchemy.sql import exists
 from ressources.webdriver import Chrome, Firefox #fichier selenium_driver.py à placer dans le même dossier que votre script
 from ressources.documentation import Documentation # fichier documentation.py qui se trouve dans le dossier ressources
 from ressources.db import * #fichier db.py  qui se trouve dans le dossier ressources
 
+
 def saveData(browser, filename_prefix='selenium'):
     '''Fonction pour l'exemple qui enregistre le code client, la capture d'écran et code serveur'''
-    browser.clientCode('./results/getCodes/codes'+filename_prefix+'_clientCode.html')
-    browser.serverCode('./results/getCodes/codes'+filename_prefix+'_serverCode.html')
-    browser.screenshot('./results/getCodes/screenshots/'+filename_prefix+'_screenshot.png', width=1080) #on fixe la largeur de la fenêtre avec width
+    browser.clientCode('./results/getCodes/codes/'+filename_prefix+'_clientCode.html')
+    browser.screenshot('./results/getCodes/screenshots/' + filename_prefix + '_screenshot.png', width=1080)  # on fixe la largeur de la fenêtre avec width
+    browser.serverCode('./results/getCodes/codes/'+filename_prefix+'_serverCode.html')
+
 
 if __name__ == '__main__':
+
     doc = Documentation()#Crée la documentation
     # ~~~~~~~~~~~~~~~ Début Selenium ~~~~~~~~~~~~~~~ #
     browser = Firefox(tor=False, headless=False)
 
     # ~~~~~~~~~~~~~~~ Récupération des URL à parcourir ~~~~~~~~~~~~~ #
-    # TODO:  Solve for this SQL expression, column, or mapped entity expected - got '<class 'ressources.db.Url'>'
-    for i in session.query(Url_ads).filter_by(status=0):
+    for i in session.query(Urls_ads).filter_by(status=0):
         #Pour tous les urls dans la database qui ne sont pas encore extrait
-        doc.info['selenium'] = []
-        info = browser.get(i.url)
-        doc.info['selenium'].append(info)
-        saveData(browser, filename_prefix=str(i.id))
-
-
-
-        with open('./results/documentation/'+str(i.id)+'_documentation.json', 'wb') as f:
-            f.write(str(doc).encode('utf-8'))
-        updateURL(session, i) #met à jour le status de l'URL
-        time.sleep(random.uniform(1.5, 2.5)) #attente entre 1.5 et 2.5 sec
+        if session.query(exists().where(Ads_Codes.ad_number == i.ad_number)).scalar():
+            pass
+        else:
+            doc.info['selenium'] = []  # documentation
+            info = browser.get(i.url)
+            doc.info['selenium'].append(info)  # documentation
+            saveData(browser, filename_prefix=str(i.ad_number)) #sauvgarder les codes recuperées
+            with open('./results/getCodes/documentation/'+str(i.ad_number)+'_documentation.json', 'wb') as f:
+                f.write(str(doc).encode('utf-8'))
+            entry = Ads_Codes(ad_number=i.ad_number, client_code =str(i.ad_number)+'_clientCode.html', server_code=str(i.ad_number)+'_serverCode.html')
+            entry.insertCode(session)
+            Urls_ads.urls_ads_update(session, i) #met à jour le status de l'URL
+            time.sleep(random.uniform(1.5, 2.5)) #attente entre 1.5 et 2.5 sec
--- ressources/db.py
+++ ressources/db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 # author: T. Pineau
+# adapte par: Danny Kohler, Luisa Rodrigues, Jasmin Wyss
 # creation: 06.10.2020
 
 import datetime
@@ -50,9 +51,9 @@ class Urls_ads(Base):
         session.add(self)
         session.commit()
 
-    def update(self, session, newStatus=1):
+    def urls_ads_update(self, session, newStatus=1):
         self.status = newStatus
-        session.commit()
+        self.commit()
 
 class Country(Base) :
     __tablename__ = 'countries'
@@ -66,7 +67,7 @@ class Country(Base) :
 
     def update(self, session, newStatus=1):
         self.status = newStatus
-        session.commit()
+        self.commit()
 
 class Ads_Codes(Base):
     __tablename__='ads_codes'
@@ -76,23 +77,23 @@ class Ads_Codes(Base):
     date_updated = Column(DateTime, onupdate=datetime.datetime.now())
     client_code = Column(String, nullable=False)
     server_code = Column(String, nullable=False)
+    #country_id = Column(Integer, ForeignKey("urls_ads.country_id")) #pas possible d'avoir deux foreign keyes
     status = Column(Integer, default=0)
 
     urls_ads = relationship("Urls_ads", backref="ads_codes")
-
     def insertCode(self, session):
         session.add(self)
         session.commit()
 
     def update(self, session, newStatus=1):
         self.status = newStatus
-        session.commit()
+        self.commit()
 
 
 #~~~~~~~~~~~~~~~~~~~~~Connect the database~~~~~~~~~~~~~~~~~~~~~
 
-engine = create_engine('sqlite:///../DATABASES/project.db') #, echo=True pour les log
-#j'ai changé ../ pour que ça crée la DB sinon ça marchait pas car pas dans le même dossier
+engine = create_engine('sqlite:///DATABASES/project.db') #, echo=True pour les log
+    #j'ai changé ../ pour que ça crée la DB sinon ça marchait pas car pas dans le même dossier [a faire entre Luisa&autres]
 Base.metadata.create_all(engine) #Create the database if it does not exist
 Session = sessionmaker(bind=engine)
 session = Session()
