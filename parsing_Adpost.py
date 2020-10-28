56a89f5f9e5dc3a7cf3b7379e058a203799ee04e
leonlopez6 <leon.lopez@unil.ch>
2020-10-28 12:33:33 +0100

    Update parsing_Adpost.py

--- parsing_Adpost.py
+++ parsing_Adpost.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: L.Lopez
+# creation: 25.10.2020
+# But: parser les pages web
+
 import time, json, random
 from sqlalchemy.sql import exists
 from ressources.webdriver import Chrome, Firefox #fichier selenium_driver.py à placer dans le même dossier que votre script
@@ -5,11 +11,13 @@ from ressources.documentation import Documentation # fichier documentation.py qu
 from ressources.db import * #fichier db.py  qui se trouve dans le dossier ressources
 from selenium import webdriver
 import re
+
+
 def main():
 
 
     driver= webdriver.Firefox(executable_path=r"webdrivers/geckodriver")
-    urls = open("results\getArticles\2020-10-21_0-59_urlArticles_documentation.json", “r”) # c'est un exemple en attendant le résultat getCodes 
+    urls = open("results\getArticles\2020-10-21_0-59_urlArticles_documentation.json", “r”) # c'est un exemple en attendant le résultat getCodes
     for url in urls:
         driver.get(url)
 
@@ -22,11 +30,10 @@ def main():
             test = test.replace('\n', '')
             category = re.search("Category\:(.*).+?(?=Ad+\s+Number)", test).group(1) ## https://stackoverflow.com/questions/7124778/how-to-match-anything-up-until-this-sequence-of-characters-in-a-regular-expres
 
-            list = ['Category', 'Ad+\s+Number', 'Description'] # c'est à continuer avec les autres éléments à parser
+            list = ['Reply+\s+to+\s+Ad','Category', 'Ad+\s+Number', 'Description', 'Breed','Age','Sex','Primilary+\s+Color','Secondary+\s+Color','Advertiser','Price','Payment+\s+Forms','Estimated+\s+Shipping','Posted+\s+By|Contact+\s+Information','Name','Zip+\s+Code|Postal+\s+Code|Post+\s+Code ','City','State+\s+>+\s+District|State+\s+>+\s+City|State+\s+>+\s+County|Province+\s+>+\s+County|Province+\s+>+\s+City|Region+\s+>+\s+County|Region|County|State+\s+>+\s+Metro','Country','Email'] # Reply to AD is to get the title, Posted by is for the username
             parse_data = []
             for idx, ele in enumerate(list):
                 print(idx)
-                import pdb; pdb.set_trace()
                 regex_ = "{}\:(.*).+?(?={})".format(ele, list[idx+1])
                 tag = re.search(regex_, test).group(1)
                 print(tag)
@@ -41,6 +48,11 @@ def main():
                 parse_data[ele] = tag
 
                 return parse_Data
+       ### ajouter le titre et le nom d'utilisatueur
+    #titre = driver.find_element_by_xpath("//font").text
+    #parse_Data.append(titre)
+    #id_user = driver.find_element_by_xpath("//td[@class='text-center']")
+    #parse_Data.append(id_user)
 
 def findMails(string):
     '''Récupère des adresses emails dans une chaine de caractère'''
