9a16396011e79abc28033a0e55135cdb63666ec2
leonlopez6 <leon.lopez@unil.ch>
2020-11-02 18:27:29 +0100

    Update parsing_Adpost.py

--- parsing_Adpost.py
+++ parsing_Adpost.py
@@ -7,26 +7,43 @@
 import time, json, random
 from ressources.webdriver import Chrome, Firefox #fichier selenium_driver.py à placer dans le même dossier que votre script
 from ressources.documentation import Documentation # fichier documentation.py qui se trouve dans le dossier ressources
-from ressources.db import Parse_ads, session
 from selenium import webdriver
+from ressources.db import Parse_ads, session
 import re
 import pdb
 
 driver = webdriver.Firefox(executable_path=r"webdrivers/geckodriver")
-set=driver.get("https://www.adpost.com/us/pets/553087/")
+set=driver.get("https://www.adpost.com/us/pets/553216/")
 
 balise_b = driver.find_element_by_xpath("//b")
 test = balise_b.find_element_by_xpath("./..").find_element_by_xpath("//div").text
 test = test.strip()
-test = test.replace("\n"," ")
-print (test)
+test = test.replace("\n"," ").replace(": "," ").replace("click above for more info on this user","").replace("Please quote Adpost when calling","").replace("click to view 1 more image","").replace("click to view 2 more images","")
+#test = test.replace(":"," ")
+#test = test.replace("click above for more info on this user","")
+#test = test.replace("Please quote Adpost when calling","")
+#test = test.replace("click to view 1 more image","")
+#test = test.replace("click to view 2 more images","")
+#res = test.replace('a', '%temp%').replace('b', 'a').replace('%temp%', 'b')
+#print (test)
+#import pdb; pdb.set_trace()
 
 
 
-liste2 = ['Reply to Ad','Category', 'Ad Number', 'Description', 'Breed','Age', 'Sex',' Primilary Color', 'Secondary Color','Advertiser','Price','Payment Forms','Estimated Shipping', 'Estimated Shipping £',' Estimated Shipping $','Estimated Shipping Rs','Posted by','Contact information',' Name', 'Company', 'Address', 'Postal Code',\
-'Zip Code', 'Post Code', 'City', 'State > District', 'State > City', 'State > County', 'Province > County', 'Province > City','Region > County', 'Region', 'County', 'State > Metro', 'Country', 'Phone','Email','Forum']
 
-clean_list = [x for x in liste if x in test] #comprehensive list
+
+list_mot = ['Reply to Ad','Category', 'Ad Number','Date Posted','Description', 'Breed','Age', 'Sex',' Primary Color', 'Secondary Color','Advertiser','Price','Payment Forms','Estimated Shipping','Posted By','Contact Information',' Name', 'Company', 'Address', 'Postal Code',\
+'Zip Code', 'Post Code', 'State > District', 'State > City','City', 'State > County','Province > County', 'Province > City','Region > County','County','Region', 'State > Metro', 'Country', 'Phone', 'Email','Forum']
+
+clean_list = [x for x in list_mot if x in test] #comprehensive list / liste de mot réelleemnt présent sur la page parmi toutes les possibilités de list_mot
+#import pdb; pdb.set_trace()
+
+if 'Province > City' in test or 'State > City' in test: #if "x" in test or "y" in test or "z" in test: print(x)
+    clean_list.remove('City')
+#import pdb; pdb.set_trace()
+if 'Region > County' in test or 'State > County' in test or 'Province > County' in test:
+    clean_list.remove('County')
+#import pdb; pdb.set_trace()
 
 parse_data = {}
 for idx, ele in enumerate(clean_list):
@@ -36,61 +53,54 @@ for idx, ele in enumerate(clean_list):
         regex_ = "{}(.*){}".format(ele, clean_list[idx+1])
         tag = re.search(regex_, test).group(1)
         parse_data[ele] = tag
-        #parse_data.append(tag)
-        print (parse_data)
+        #print (parse_data)
 
-
+# rechercher le numéro de téléphone, l'adresse email ainsi qu'un site web dans le champ "Description"
+# et ajout de ces informations au dictionnaire
 description = parse_data.get('Description')
 email, phone, website = None, None, None
 if description:
-    #regex
-    #phone = re.findall()
+
     email_regex = re.search(r"((?:[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))",description)
     if email_regex:
         email = email_regex.group(1)
         print (email)
 
-    import pdb; pdb.set_trace()
     phone_regex = re.search(r"(((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7})))",description)
     if phone_regex:
         phone = phone_regex.group(1)
         print (phone)
 
-    website= re.search(r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})",description)
+    website_regex = re.search(r"((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}))",description)
     if website_regex:
         website = website_regex.group(1)
         print (website)
 
 
-
-    #parse_data['phone'] = phone
     parse_data['Email'] = email
+    parse_data['phone'] = phone
+    parse_data['redirect_website'] = website
     del parse_data['Description']
+    new_key = "title"
+    old_key = "Reply to Ad"
+    parse_data[new_key]= parse_data.pop(old_key)
+    state = ('State > District', 'State > City','State > County','Province > County','Province > City', 'Region > County', 'Region','County', 'State > Metro')
+    for x in state:
+        if x in parse_data:
+            parse_data["State"] =parse_data.pop(x)
 
-    print(parse_data)
+    #for e in ['cc', 'dd',...]:
+    #parse_data["Canton"] =parse_data.pop(e)
+    #parse_data["Canton"] = parse_data.pop('State > County' or )
+    #import pdb; pdb.set_trace()
 
-    #if ele == "Description":
-                    #mail=findMails(ele)
-                    #parse_data.append(mail)
+    #print(parse_data)
 
-    #if test.find(ele) != -1:  #https://www.programiz.com/python-programming/methods/string/find
-        #continue
-    #else:
-        #idx += 1
-        #parse_data.append("None")
-        #import pdb; pdb.set_trace()
 
 
 
 print (parse_data)
-#essai= ["la","2","4"]
-#Parse_ads(title=essai[0], category=essai[1], ad_number= essai[2]).insertParse_ads(session)
+# sqlite
 
-
-def findMails(string):
-    '''Récupère des adresses emails dans une chaine de caractère'''
-    result = re.findall(r'+', str)
-    #result = re.findall("\\b[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*(?:[\s\[\(])*(?:@|at)(?:[\s\]\)])*(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", string)
-
-    return result
-    #regex_ = "{}\:(.*).+?(?={})".format(ele, list[idx+1]
+#Parse_ads(title=parse_data.get('title'), category= parse_data.get('Category'), ad_number= parse_data.get('Ad Number'), breed = parse_data.get('Breed'), age = parse_data.get('Age'), sex= parse_data.get('Sex'), primary_color = parse_data.get('Primary Color'), \
+#secondary_color= parse_data.get('Secondary Color'), advertiser= parse_data.get('Advertiser'), price= parse_data.get('Price'), ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')ad_number= parse_data.get('Ad Number')).insertParse_ads(session)
